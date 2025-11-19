7073129d9ae8d2483af3498c6399021f46428e76
Roman Krasinskij <justromankrasinskij@gmail.com>
2025-11-19 10:27:16 +0300

    Edit Spring Boot classes Edited classes: - StaffTaskSpaceApplication.java - StaffTaskSpaceApplicationTests.java

--- src/main/java/ru/romankrasinskij/stafftaskspace/StaffTaskSpaceApplication.java
+++ src/main/java/ru/romankrasinskij/stafftaskspace/StaffTaskSpaceApplication.java
@@ -2,12 +2,11 @@ package ru.romankrasinskij.stafftaskspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.romankrasinskij.stafftaskspace.services.RegistrationService;
 
 @SpringBootApplication
 public class StaffTaskSpaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StaffTaskSpaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StaffTaskSpaceApplication.class, args);
+    }
 }
