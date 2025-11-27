dc627fad1775daa600787acb895625ce3363c31e
Roman Krasinskij <justromankrasinskij@gmail.com>
2025-11-27 15:37:30 +0300

    Add LoginController class

---
+++ src/main/java/ru/romankrasinskij/stafftaskspace/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package ru.romankrasinskij.stafftaskspace.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login-form";
+    }
+
+    @GetMapping("/")
+    public String redirect() {
+        return "redirect:/login";
+    }
+}
