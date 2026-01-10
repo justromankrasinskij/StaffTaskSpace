01f5090b5d6e12d3ea33b8f2f93db8de0c72f2d1
Roman Krasinskij <justromankrasinskij@gmail.com>
2026-01-10 12:13:20 +0300

    Rename services package

--- src/main/java/ru/romankrasinskij/stafftaskspace/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/romankrasinskij/stafftaskspace/service/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.romankrasinskij.stafftaskspace.service;
 
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
